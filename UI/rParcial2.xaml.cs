dd6c63e34b0c0b20b33d7098631fd9b49d27e080
Niurbis Del Carmen Reyes Jaquez <niurbis_reyes@ucne.edu.do>
2020-03-16 17:51:25 -0400

    Add project files.

---
+++ UI/rParcial2.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Parcial2.UI
+{
+    /// <summary>
+    /// Interaction logic for rParcial2.xaml
+    /// </summary>
+    public partial class rParcial2 : Window
+    {
+        public rParcial2()
+        {
+            InitializeComponent();
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoverButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AgregarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
